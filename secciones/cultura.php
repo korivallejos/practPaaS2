ab1535881a25166b906bf05b6b1484aa5f657528
korivallejos <99447436+korivallejos@users.noreply.github.com>
2024-01-09 22:16:57 -0500

    cultura.php

--- secciones/cultura.php
+++ secciones/cultura.php
@@ -4,8 +4,10 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $cultura = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "
+Revista del Lugar de la Memoria, la Tolerancia y la Inclusión Social +Memoria(s)",
+"autor" => "Ministerio de cultura",
+"resumen" => "El Lugar de la Memoria, la Tolerancia y la Inclusión Social (LUM), como espacio de memoria nacional sobre los hechos de violencia sucedidos durante el periodo 1980-
+2000, es un lugar de encuentro de las diversas memorias en el Perú. En cumplimiento de su misión, el LUM reconoce la necesidad de impulsar mecanismos que fomenten el diálogo entre académicos que producen investigaciones sobre el periodo de violencia, entre otros instrumentos que permitan conocer con mayor profundidad los hechos ocurridos.",
 ];
 ?>
