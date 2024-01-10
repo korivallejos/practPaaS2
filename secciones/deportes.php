cda664358ce102be19c58e43b2e9b3d7a5ad54f6
korivallejos <99447436+korivallejos@users.noreply.github.com>
2024-01-09 22:17:29 -0500

    deportes.php

--- secciones/deportes.php
+++ secciones/deportes.php
@@ -2,10 +2,9 @@
 /*****
 SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
-
 $deportes = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "PD Deportes",
+"autor" => "Lopez Aliaga",
+"resumen" => "¡Llegó la edición 125 de la revista PD DEPORTES - tu dosis de acción mensual!",
 ];
 ?>
