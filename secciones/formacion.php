f896b663d44aced9acf8f26b21e36a370688de4e
korivallejos <99447436+korivallejos@users.noreply.github.com>
2024-01-09 22:19:18 -0500

    formacion.php

--- secciones/formacion.php
+++ secciones/formacion.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $formacion = [
-"titulo" => "XXX",
-"autor" => "Gabinete de comunicación",
-"resumen" => "XXX",
+"titulo" => "FORMACIÓN UNIVERSITARIA",
+"autor" => "Gabinete de eduación",
+"resumen" => "Las revistas educativas son un recurso educativo con el que los niños pueden reforzar lo aprendido en la escuela e incluso aprender muchas cosas nuevas de mucha utilidad,",
 ];
 ?>
