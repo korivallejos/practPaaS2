5ff81b991b2b5f9098d7c3b6ce941c7dbcc6de6f
korivallejos <99447436+korivallejos@users.noreply.github.com>
2024-01-09 22:17:41 -0500

    economia.php

--- secciones/economia.php
+++ secciones/economia.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $economia = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "Revista Estudios Económicos",
+"autor" => "Pedro Lopez",
+"resumen" => "La Revista Estudios Económicos (REE) es una publicación del Banco Central de Reserva del Perú que tiene como objetivo la divulgación de investigaciones económicas en temas de interés para el Banco Central de Reserva del Perú y la economía peruana. Las principales áreas de interés de la REE incluyen macroeconomía, política monetaria, economía internacional, política económica y finanzas.",
 ];
 ?>
