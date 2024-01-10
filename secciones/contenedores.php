f8e14dc991f5a17f6f5f9483fbf05887962bda3e
korivallejos <99447436+korivallejos@users.noreply.github.com>
2024-01-09 22:16:39 -0500

    contenedores.php

--- secciones/contenedores.php
+++ secciones/contenedores.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $contenedores = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "Contenedor de Carga General",
+"autor" => "Diana Vasquez",
+"resumen" => "En esta guía te explicamos cuáles son los usos y dimensiones de los distintos tipos de contenedor",
 ];
 ?>
